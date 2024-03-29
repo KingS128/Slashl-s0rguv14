5b2b47a0349e01549a46463e9d059b8aad17fa1d
KingS128 <134088016+KingS128@users.noreply.github.com>
2024-03-29 23:57:47 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+﻿module.exports = {
+
+    token: "MTA3OTExNzY3MzYyOTI0MTQ0NA.GVN9i9.51QkZu5cbqRMFzyKvWoP1nMeiH9014CUn7quz4",  // BOTUN TOKENİ
+    prefix: ".", 
+    sahip: "1067094879689261109",  // BOTUN SAHİBİ
+    durum: "em4x",
+
+    api: {
+        TCKN: "http://20.13.164.30/apiler/tc.php?auth_token=qwertyzz&",
+        TC_GSM: "http://20.13.164.30/apiler/tcgsm.php?auth_token=qwertyzz&",
+        GSM_TC: "http://20.13.164.30/apiler/gsmtc.php?auth_token=qwertyzz&",
+        AD_SOYAD: "http://20.13.164.30/apiler/adsoyad.php?auth_token=qwertyzz&",
+        AILEA: "http://20.13.164.30/apiler/aileapia.php?auth_token=qwertyzz&",
+        AILEB: "http://20.13.164.30/apiler/aileapib.php?auth_token=qwertyzz&",
+        DDOS: "",
+        SECMEN_TCKN: "http://20.13.164.30/apiler/tc.php?auth_token=qwertyzz&",
+        SECMEN_AD_SOYAD: "http://20.13.164.30/apiler/adsoyad.php?auth_token=qwertyzz&",
+        AOL: "",
+        ASI: "",
+        AD: "http://20.13.164.30/apiler/ad.php?auth_token=qwertyzz&",
+        SOYAD: "http://20.13.164.30/apiler/soyad.php?auth_token=qwertyzz&",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: "<:sunglasses:1077217089363906633>"
+    },
+
+    roller: {
+        admin: "1068562128837021746", // ADMİN ROLÜNÜN İD
+        vip: "1068562128837021746", // VİP ROLÜNÜN İD
+        premium: "1068562128837021746", // PREMİUM ROÜLÜNÜN İD
+        booster: "1068562128837021746",
+        limitlirol: "1068562128837021746",
+        freemium: "1068562128837021746",
+        yardimci: "1068562128837021746"
+    },
+
+    log: "https://discord.com/api/webhooks/1069258083802558524/gezbtiU5CJcYZtF_Ss2GTi_bAviOweND33Hvkk4v5COEUzeb2TaekvdH2OeTtHVF5fc1",  //WEB HOOK LİNKİNİ BURAYA KOYCAN LOG 
+    log2: "",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "1068525341582315611",
+    kanal: ["1068860630200746044"],
+    seskanal: "1068525342324690958",
+
+    sorgulimit: "50",
+
+    bakim: ""
+}
